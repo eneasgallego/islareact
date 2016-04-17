04b2810191ae56c3bfab8542f8a53d33c6391296
Eneas Gallego <eneasgallego@gmail.com>
2016-04-17 19:39:59 +0200

    Nuevo orden

--- jsx/index.jsx
+++ jsx/index.jsx
@@ -178,6 +178,15 @@ let options = {
             titulo: 'Materiales',
             url: 'http://localhost:3000/db',
             orden:  [{
+                campo(item) {
+                    return ((item.stockmateriales < item.cantidadpedidos) &&
+                    (item.stockmateriales + item.haciendomateriales < item.cantidadpedidos) &&
+                    (item.maximofabricas != -1) &&
+                    (item.haciendofabricas < item.maximofabricas) &&
+                    (item.faltanecesita)) ? 1 : 0;
+                },
+                desc: true
+            },{
                 campo: "profundidadpedidos",
                 desc: true
             },{
